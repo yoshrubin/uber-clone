9f8265892bc45a38600a34da0bfaf23938dc999a
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-02 22:24:18 +0300

    add store to App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>UBER</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>UBER</Text>
+      </View>
+    </Provider>
   );
 }
 
