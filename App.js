a30cc1e09b23deb7c43b9866a95db8d95188cb28
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-03 07:07:26 +0300

    started implementing navigation

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
+import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
 import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="MapScreen" component={MapScreen} />
+          </Stack.Navigator>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
 
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -9,6 +9,7 @@ import {
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -26,13 +27,18 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`pr-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`pr-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
