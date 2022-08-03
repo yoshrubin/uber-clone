feffcc77268ea2aec51a46d5d53934bc81348293
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-03 21:50:21 +0300

    add destination

---
+++ components/NavigateCard.js
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={tw`flex-1 bg-white`}>
+      <Text style={tw`text-center py-5 text-xl`}>NavigateCard</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={{
+              container: {
+                flex: 0,
+                backgroundColor: "white",
+                paddingTop: 20,
+              },
+              textInput: {
+                backgroundColor: "#DDDDDF",
+                borderRadius: 0,
+                fontSize: 18,
+              },
+              textInputContainer: {
+                paddingHorizontal: 20,
+                paddingBottom: 0,
+              },
+            }}
+            query={{
+              key: GOOGLE_MAPS_KEY,
+              language: "en",
+            }}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                }),
+                navigation.navigate("RideOptionsCard")
+              );
+            }}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            minLength={2}
+            placeholder="Where To?"
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
+
+const styles = StyleSheet.create({});
