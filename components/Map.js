bf240df114fe6b4dec0403e1bfda7e8f21e05919
Yoshi Rubin <yoshrubin@gmail.com>
2022-08-04 09:01:26 +0300

    zoom out to fit both markers in map

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
 import { useSelector } from "react-redux";
@@ -10,9 +10,19 @@ import { GOOGLE_MAPS_KEY } from "@env";
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    // Zoom and fit to markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
@@ -40,6 +50,17 @@ const Map = () => {
           title="Travel From"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Travel To"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
